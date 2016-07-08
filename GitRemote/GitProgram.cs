af9d8010830ef1ea65b706fcdeadf9fe69265b0c
Adam Dunigan <adunigan@accruent.com>
2016-07-08 12:55:33 -0500

    update

---
+++ GitRemote/GitProgram.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace GitRemote
+{
+    public class GitProgram
+    {
+        public string Command { get; set; }
+        public string WorkingDirectory { get; set; }
+        public ProcessStartInfo gitInfo { get; set; }
+        public string GitExeLocation { get; set; }
+        public Process gitProcess { get; set; }
+
+        public GitProgram(string cmd, string dir)
+        {
+            WorkingDirectory = dir;
+            Command = cmd;
+            GitExeLocation = @"C:\Program Files\Git\bin\git.exe";
+            gitInfo = new ProcessStartInfo();
+            gitInfo.CreateNoWindow = true;
+            gitInfo.RedirectStandardError = true;
+            gitInfo.RedirectStandardOutput = true;
+            gitInfo.UseShellExecute = false;
+            gitInfo.FileName = GitExeLocation;
+            gitInfo.WorkingDirectory = dir;
+            gitProcess = new Process();
+        }
+
+        public void GitCommit()
+        {
+            gitInfo.Arguments = "add *";
+            gitProcess.StartInfo = gitInfo;
+            gitProcess.Start();
+            gitProcess.WaitForExit();
+
+            gitInfo.Arguments = "commit -m 'Generated automatically fom WebSocket_Home'";
+            gitProcess.StartInfo = gitInfo;
+            gitProcess.Start();
+            gitProcess.WaitForExit();
+        }
+
+        public void GitPush()
+        {
+            gitInfo.Arguments = "push --all origin";
+            gitProcess.StartInfo = gitInfo;
+            gitProcess.Start();
+            gitProcess.WaitForExit();
+        }
+
+        public void GitPull()
+        {
+            gitInfo.Arguments = "pull";
+            gitProcess.StartInfo = gitInfo;
+            gitProcess.Start();
+            gitProcess.WaitForExit();
+        }
+
+        public void DoIt()
+        {
+            if (Command == "pull")
+            {
+                GitPull();
+            }
+            else if (Command == "push")
+            {
+                GitPush();
+            }
+            else if (Command == "commit")
+            {
+                GitCommit();
+            }
+            gitProcess.Close();
+        }
+    }
+}
--- GitRemote/Program.cs
+++ GitRemote/Program.cs
@@ -1,42 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
 namespace GitRemote
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            ProcessStartInfo gitInfo = new ProcessStartInfo();
-            gitInfo.CreateNoWindow = true;
-            gitInfo.RedirectStandardError = true;
-            gitInfo.RedirectStandardOutput = true;
-            gitInfo.FileName = @"C:\Program Files\Git\bin\git.exe";
-
-            Process gitProcess = new Process();
-            if (args[0] == "pull")
-            {
-                gitInfo.Arguments = "pull";
-            }
-            else if (args[0] == "push")
-            {
-                gitInfo.Arguments = "push --all origin";
-            }
-            gitInfo.WorkingDirectory = args[1];
-
-            gitInfo.UseShellExecute = false;
-
-            gitProcess.StartInfo = gitInfo;
-            gitProcess.Start();
-
-            string stderr_str = gitProcess.StandardError.ReadToEnd();
-            //string stdout_str = gitProcess.StandardOutput.ReadToEnd(); // pick up STDOUT
-
-            gitProcess.WaitForExit();
-            gitProcess.Close();
+            
         }
     }
 }
